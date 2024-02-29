bea40b88e5dacb8ba7628a5f0f07603043badbe1
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-03-01 00:07:44 +0300

    Add files via upload

---
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.user;
+
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.user.dto.UserDto;
+
+@Mapper
+@Component
+public class UserMapper {
+    public User toUserDto(UserDto userDto) {
+        if (userDto == null) {
+            return null;
+        }
+
+        return User.builder()
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .build();
+    }
+}
+
