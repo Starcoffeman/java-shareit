bbdfd7f3a9b2b1d63292673f59a717b286a477fa
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-02-29 19:09:51 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -3,11 +3,8 @@ package ru.practicum.shareit.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-
 
 @RestControllerAdvice
 @Slf4j
