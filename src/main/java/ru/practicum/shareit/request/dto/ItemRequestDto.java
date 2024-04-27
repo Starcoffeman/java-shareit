c6de0b4e5d1977480f5a70c9f2f8768191863bf0
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-04-27 03:26:34 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,8 +1,9 @@
 package ru.practicum.shareit.request.dto;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.intf.Create;
 import ru.practicum.shareit.item.dto.ItemDto;
 
@@ -13,6 +14,8 @@ import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequestDto {
 
     private long id;
