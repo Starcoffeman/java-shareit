e0a618888b729cf3a30d14ce21a055bdf5da515b
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-04-27 03:30:26 +0300

    Add files via upload

--- src/test/java/ru/practicum/shareit/comment/CommentServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/comment/CommentServiceImplTest.java
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
