43a7c8f07cf791859906eb2585d49cc0fd0ce1d3
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-05-06 00:26:36 +0300

    Add files via upload

--- src/test/java/ru/practicum/shareit/booking/BookingMapperTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingMapperTest.java
@@ -2,19 +2,23 @@ package ru.practicum.shareit.booking;
 
 import org.junit.jupiter.api.Test;
 import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BookingMapperTest {
 
     @Test
-    void mapToBookingDto_WhenBookingIsValid_ShouldReturnBookingDto() {
+    void mapToBookingDtoWhenBookingIsValid_ShouldReturnBookingDto() {
         User booker = new User();
         booker.setId(1L);
 
@@ -37,6 +41,32 @@ class BookingMapperTest {
         assertEquals(booking.getEnd(), result.getEnd());
         assertEquals(booking.getBooker().getId(), result.getBooker().getId());
         assertEquals(booking.getItem().getId(), result.getItem().getId());
+        assertEquals(booking.getStatus(), result.getStatus());
+    }
+
+    @Test
+    void mapToBookingRequestDto() {
+        UserDto booker = new UserDto();
+        booker.setId(1L);
+
+        ItemDto item = new ItemDto();
+        item.setId(2L);
+
+        BookingDto booking = new BookingDto();
+        booking.setId(3L);
+        booking.setStart(LocalDateTime.now());
+        booking.setEnd(LocalDateTime.now().plusDays(1));
+        booking.setBooker(booker);
+        booking.setItem(item);
+        booking.setStatus(BookingStatus.APPROVED);
+
+        BookingRequestDto result = BookingMapper.mapToBookingRequestDto(booking);
+        assertNotNull(result);
+        assertEquals(booking.getId(), result.getId());
+        assertEquals(booking.getStart(), result.getStart());
+        assertEquals(booking.getEnd(), result.getEnd());
+        assertEquals(booking.getBooker().getId(), result.getBookerId());
+        assertEquals(booking.getItem().getId(), result.getItemId());
         assertEquals(booking.getStatus(), result.getStatus());
     }
 }
